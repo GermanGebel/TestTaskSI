4c1ea9a32a0609ff1b74a2e7f74705d4b11177b3
German <gebel.german@yahoo.com>
2022-04-21 09:42:26 +0300

    add test logs

--- task_1/test_data.py
+++ task_1/test_data.py
@@ -10,5 +10,7 @@ test_data = [
         (ADD, 7), (ADD, 8), (ADD, 9), (OUT, 6), (OUT, 3), (OUT, 1), (OUT, 2), (OUT, 7)], 18),
     ([(ADD, 1), (ADD, 2), (ADD, 3), (ADD, 4), (OUT, 1), (ADD, 5), (ADD, 6), 
         (ADD, 7), (ADD, 8), (ADD, 9), (OUT, 6), (OUT, 3), (OUT, 2), (OUT, 7)], 20),
-    ([(ADD, 1), (ADD, 2), (ADD, 3), (ADD, 4), (ADD, 5), (OUT, 4), (OUT, 1), (OUT, 5), (OUT, 3), (OUT, 2)], 16)
+    ([(ADD, 1), (ADD, 2), (ADD, 3), (ADD, 4), (ADD, 5), (OUT, 4), (OUT, 1), (OUT, 5), (OUT, 3), (OUT, 2)], 16),
+    ([(ADD, 1), (ADD, 2), (ADD, 5), (ADD, 4), (ADD, 3), (OUT, 4), (OUT, 1), (OUT, 5), (OUT, 3), (OUT, 2)], 14),
+    ([(ADD, 1), (ADD, 2), (ADD, 3), (ADD, 4), (ADD, 5), (OUT, 4), (OUT, 3), (OUT, 5), (OUT, 2), (OUT, 1)], 16),
 ]
